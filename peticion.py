ddf6f2ba4165fa23e4444bac162d94cbf28021b3
juliana <juli@Mercury.(none)>
2012-07-15 10:50:13 -0430

    peticion bien

--- peticion.py
+++ peticion.py
@@ -4,20 +4,21 @@ class Peticion(object):
 
     
 
-    def __init__(self, tipoP, nombreArch,version):
+    def __init__(self, tipoP, nombreArch,version,archi):
 	self.tipoPeticion = tipoP
 	self.nombreArch = nombreArch
 	self.version = version
+	self.archi= archi
 	# faltaria el archivo como tal si es commit
 	
 	
     def enviarRequest(self,switch):
 	print("Estoy en peticion, {0} ".format(self.tipoPeticion))
 	#	tipoP = self.tipoPeticion
-	switch.guardalo(self.tipoPeticion,self.nombreArch,self.version)
+	switch.guardalo(self.tipoPeticion,self.nombreArch,self.version,self.archi)
 	
     def enviarCosas(self,resolv):
 	print("Estoy en peticion, {0} ".format(self.tipoPeticion))
-	resolv.guardalos(self.tipoP,self.nombreArch,self.version)
+	resolv.guardalos(self.tipoPeticion,self.nombreArch,self.version)
 	
     
