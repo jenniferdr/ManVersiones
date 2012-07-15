7223bcdb3704a877bc8611e329acbdece95c5477
juliana <juli@Mercury.(none)>
2012-07-15 16:29:51 -0430

    cliente casi listo

--- client.py
+++ client.py
@@ -5,59 +5,47 @@ from peticion import Peticion
 
 print("# agumentos {0}.".format(len(sys.argv)))
 version = ""
-if(len(sys.argv) < 3 or len(sys.argv) >4):
-    print("Sintaxis: client <accion> <archivo> [version-para update]")
+if(len(sys.argv) < 4 or len(sys.argv) >5):
+    print("Sintaxis: client <accion> <archivo> <ipSwitch> [version-para update]")
     exit()
 
-elif(len(sys.argv) == 3):
+tipoP = sys.argv[1]
+archivo = sys.argv[2]
+ip= sys.argv[3]
+sys.excepthook=Pyro4.util.excepthook
+switch=Pyro4.Proxy('PYRO:example.switch@'+ip+':5000')
+    
+if(len(sys.argv) == 4):
     if(sys.argv[1]=="commit"):
-		tipoP = sys.argv[1]
-		archivo = sys.argv[2]
-		try:
-	
-				f = open(archivo)
-				lectura = f.read()    
-				switch=Pyro4.Proxy('PYRO:example.switch@201.211.164.206:8080')
-
-				pet1 = Peticion(tipoP,archivo,version,lectura)
-				pet1.enviarRequest(switch)
-	
-		except IOError:
-			print 'No existe', archivo
+      try:
+         f = open(archivo)
+         lectura = f.read()    
+         pet1 = Peticion(tipoP,archivo,1,lectura)
+         pet1.enviarRequest(switch)
+      except IOError:
+         print 'No existe', archivo
 			
-	# guardar los otros parametros
-			#	print ("commit 3")
-    elif(sys.argv[1]=="checkout"):
-	tipoP = sys.argv[1]
-	archivo = sys.argv[2]
-	# guardar los otros parametros.	
-	print ("checkout 3")
-    elif(sys.argv[1]=="update"):
-	tipoP = sys.argv[1]
-	archivo = sys.argv[2]
-	print ("update 3")
-	# guardar los otros parametros.
+    elif(sys.argv[1]=="checkout" or sys.argv[1]=="update"):
+         pet1 = Peticion(tipoP,archivo,0,'')
+         pet1.enviarRequest(switch)
+	
     else:
 	print("Sintaxis:client <commit|checkout|update> <archivo> [version]")
 	exit()
 	
-elif(len(sys.argv)==4):
+elif(len(sys.argv)==5):
     if(sys.argv[1]=="update"):
-	tipoP = sys.argv[1]
-	archivo = sys.argv[2]
-	version = sys.argv[3]
-	print("update 4")
-	# guardar los otros parametros.
+         version = sys.argv[4]
+         pet1 = Peticion(tipoP,archivo,version,'')
+         pet1.enviarRequest(switch)
     else:
-	print("Sintaxis update: <update> <archivo> [version]")
-	exit()
+	     print("Sintaxis update: <update> <archivo> [version]")
+	     exit()
 	
-sys.excepthook=Pyro4.util.excepthook
-
 def error(a,o):
 	if (o==0):
 		print ("Debe dar el path completo del archivo {0} | Debe existir").format(a)
-	
+		exit()
  
 			
     
